a4d0c34251ff19973c5b6f7c9d52a80e608be0cd
Abhishek Jadhav <59304331+avzard@users.noreply.github.com>
2022-07-11 15:12:59 +0530

    [Abhishek]Delete Contact

--- CompleteAddressBook/CompleteAddressBook/AddressBookMain.cs
+++ CompleteAddressBook/CompleteAddressBook/AddressBookMain.cs
@@ -12,7 +12,7 @@ namespace CompleteAddressBook
 			while (flag)
 			{
 				Console.WriteLine("******WELCOME TO ADDRESS BOOK******");
-				Console.WriteLine("1. Add_Contact \n2. Display_Contact \n3. Update_Contact\n4. Exit");
+				Console.WriteLine("1. Add_Contact \n2. Display_Contact \n3. Update_Contact\n4. Delet_Contact\n5. Exit");
 				Console.WriteLine("Enter Your Choice:");
 				int input = Convert.ToInt32(Console.ReadLine());
 				switch (input)
@@ -30,6 +30,11 @@ namespace CompleteAddressBook
 						EditContact(fname);
 						break;
 					case 4:
+						Console.Write("Enter FirstName U want to Delet : ");
+						string deletName = Console.ReadLine();
+						DeletContact(deletName);
+						break;
+					case 5:
 						flag = false;
 						break;
 					default:
@@ -109,6 +114,27 @@ namespace CompleteAddressBook
 				else if (size == check)
 				{
 					Console.WriteLine(fname + " not found in addressbook...");
+					break;
+				}
+			}
+		}
+		public static void DeletContact(string Fname)
+		{
+			int size = userList.Count;
+			int check = 0;
+			foreach (ContactPerson user in userList)
+			{
+				check++;
+				if (user.firstName.Equals(Fname))
+				{
+					userList.Remove(user);
+					Console.WriteLine("Contact Deleted Successfully...");
+					Display();
+					break;
+				}
+				else if (size == check)
+				{
+					Console.WriteLine(Fname + " not found in addressbook...");
 					break;
 				}
 			}
