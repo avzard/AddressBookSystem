364360dd6a4b33906105596a1ca4ca4edfc4deb2
Abhishek Jadhav <59304331+avzard@users.noreply.github.com>
2022-07-11 15:39:43 +0530

    [Abhishek]View Person

--- CompleteAddressBook/CompleteAddressBook/MultipleAddressBook.cs
+++ CompleteAddressBook/CompleteAddressBook/MultipleAddressBook.cs
@@ -111,16 +111,24 @@ namespace CompleteAddressBook
 		}
 		public void SerchContact(string place)
 		{
+			List<string> person = new List<string>();
 			bool exits = isPlaceExist(place);
 			if (exits)
 			{
+				Console.WriteLine("Contacts From Place: " + place);
 				foreach (ContactPerson user in userList.FindAll(x => x.address.Equals(place)).ToList())
 				{
-					user.print();
+					string name = user.firstName + " " + user.lastName;
+					person.Add(name);
 				}
 				foreach (ContactPerson user in userList.FindAll(x => x.state.Equals(place)).ToList())
 				{
-					user.print();
+					string name = user.firstName + " " + user.lastName;
+					person.Add(name);
+				}
+				foreach (string val in person)
+				{
+					Console.WriteLine(val);
 				}
 			}
 			else
@@ -130,7 +138,7 @@ namespace CompleteAddressBook
 		}
 		public bool isPlaceExist(string place)
 		{
-			if (this.userList.Any(e => e.address == place) || this.userList.Any(e => e.state == place))
+			if (this.userList.Any(e => e.city == place) || this.userList.Any(e => e.state == place))
 				return true;
 			else
 				return false;
